8effda8abde0f1156d29b5f0357c1d64db21423f
Rev082 <kevin.pamart@inseec-france.com>
2019-10-29 16:55:00 +0100

    modifications kevin

--- src/main/java/jpa/repository/ClientRepository.java
+++ src/main/java/jpa/repository/ClientRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import jpa.model.Client;
 
-public interface ClientRepository extends JpaRepository<Client, Integer> {
+public interface ClientRepository extends JpaRepository<Client, Long> {
 
 }
