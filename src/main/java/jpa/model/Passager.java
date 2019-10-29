d23345f920bec60837837f85c971fb504f16c35e
sajidaayachi@gmail.com <sajidaayachi@gmail.com>
2019-10-29 11:44:01 +0100

    Ajout Model Saj

---
+++ src/main/java/jpa/model/Passager.java
@@ -0,0 +1,147 @@
+package jpa.model;
+
+import java.util.Set;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Version;
+
+@Entity
+@Table(name = "passager")
+@SequenceGenerator(name = "seqPassager", sequenceName = "seq_passager", initialValue = 100, allocationSize = 1)
+
+public class Passager {
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seqPassager")
+	@Column(name = "id_passager")
+	private Integer id;
+	@Column(name = "prenom_passager", length = 150, nullable = false)
+	private String prenom;
+	@Column(name = "nom_passager", length = 150)
+	private String nom;
+	@Embedded
+	@AttributeOverrides({@AttributeOverride(name = "adresse", column = @Column(name = "adresse_passager", length = 200)),
+			@AttributeOverride(name = "codePostal", column = @Column(name = "code_postal_passager", length = 5)),
+			@AttributeOverride(name = "ville", column = @Column(name = "ville_passager", length = 200)) ,
+			@AttributeOverride(name = "pays", column = @Column(name = "pays_passager", length = 200))})
+	private Adresse adresse;
+	
+	@OneToMany(mappedBy="passager")
+	private Set<Reservation> reservations;
+	
+	
+	public Passager() {
+
+	}
+
+		
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Passager other = (Passager) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	/**
+	 * @return the prenom
+	 */
+	public String getPrenom() {
+		return prenom;
+	}
+	/**
+	 * @param prenom the prenom to set
+	 */
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+	/**
+	 * @param nom the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	/**
+	 * @return the adresse
+	 */
+	public Adresse getAdresse() {
+		return adresse;
+	}
+	/**
+	 * @param adresse the adresse to set
+	 */
+	public void setAdresse(Adresse adresse) {
+		this.adresse = adresse;
+	}
+	/**
+	 * @return the reservation
+	 */
+
+	/**
+	 * @return the reservations
+	 */
+	public Set<Reservation> getReservations() {
+		return reservations;
+	}
+
+	/**
+	 * @param reservations the reservations to set
+	 */
+	public void setReservations(Set<Reservation> reservations) {
+		this.reservations = reservations;
+	}
+	
+	
+}
