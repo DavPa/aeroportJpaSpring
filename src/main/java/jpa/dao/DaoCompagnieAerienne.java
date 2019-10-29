284d5c30e6f96ae835376f8d96bf32009b8c830d
unknown <david.pasquier@outlook.fr>
2019-10-29 12:46:40 +0100

    dossier dao supprime

--- src/main/java/jpa/dao/DaoCompagnieAerienne.java
+++
@@ -1,7 +0,0 @@
-package jpa.dao;
-
-import jpa.model.CompagnieAerienne;
-
-public interface DaoCompagnieAerienne extends DaoGeneric<CompagnieAerienne, Long>{
-
-}
--- src/main/java/jpa/dao/DaoCompagnieAerienneVol.java
+++
@@ -1,8 +0,0 @@
-package jpa.dao;
-
-import jpa.model.CompagnieAerienneVol;
-import jpa.model.CompagnieAerienneVolPk;
-
-public interface DaoCompagnieAerienneVol extends DaoGeneric<CompagnieAerienneVol,CompagnieAerienneVolPk>{
-
-}
